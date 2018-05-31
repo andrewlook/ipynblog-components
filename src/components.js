24bfae028270e307ade196bc2f781a65b2bf77e4
Andrew Look <andrew.m.look@gmail.com>
2018-05-31 19:38:35 +0900

    working through the header inclusion

--- src/components.js
+++ src/components.js
@@ -1,9 +1,11 @@
 import PostPreview from './PostPreview.html';
 import PostsList from './PostsList.html';
+import HeaderNav from './HeaderNav.html';
 
 const components = [
     PostPreview,
     PostsList,
+    HeaderNav,
 ];
 
 export default components;
